aa5a164ac6f266c84fea3da44f6ec93b15753189
Sai Kovour <70875830+SuperSaigon123@users.noreply.github.com>
2023-09-21 08:19:00 -0500

    9/21
    worked on connectivity from camera to images

---
+++ SaiKovour_ENP/components/CamButton.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import {Text, TouchableOpacity, StyleSheet} from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default function CamButton({title, onPress, icon, color}){
+    return(
+        <TouchableOpacity onPress={onPress} style={styles.button}>
+            <MaterialCommunityIcons name={icon} color={color} size={35} />
+            <Text style={styles.text}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        height: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    text: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: '#FFFFFF',
+        marginLeft: 10,
+    }
+})
--- SaiKovour_ENP/screens/CameraScreen.js
+++ SaiKovour_ENP/screens/CameraScreen.js
@@ -1,21 +1,47 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Image, Text, View, useState, TouchableOpacity } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import { Button } from 'react-native-elements';
+import CamButton from '../components/CamButton';
 
 export default function CameraScreen() {
+    const [image, setImage] = React.useState[null]
     const [type, setType] = React.useState(CameraType.back);
     const [permission, requestPermission] = Camera.useCameraPermissions();
+
+    const cameraRef = React.useRef(null)
 
     function toggleCameraType() {
       setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
     }
 
+    const takePicture = async() => {
+      if(cameraRef) {
+        try{
+          const data = await cameraRef.current.takePictureAsync();
+          setImage[data.uri]
+        } catch(e){
+          console.log(e)
+        }
+      }
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Camera screen</Text>
-  
-        <Button title="Click me" style={styles.button} />
+        {!image ?
+        <Camera style={styles.camera}> 
+          <Text>Hello</Text>
+        </Camera>
+        :
+        <Image />}
+        <View>
+          <CamButton
+            title={'Take picture'} 
+            icon={'camera'}
+            color={'white'}
+            
+            ></CamButton>
+        </View>
       </View>
     );
   }
@@ -23,11 +49,11 @@ export default function CameraScreen() {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
+      backgroundColor: '#000',
       justifyContent: 'center',
+      paddingBottom: 120,
     },
-    button: {
-      marginTop: 10
-    }
+    camera: {
+      flex: 1,
+    },
   });
